151e193ac591df527544ca90d6b1c7838658f149
Yannis Bouhamou <yannis.bouhamou@hotmail.fr>
2020-05-19 16:06:49 +0200

    implemented pundit and routes/actions/views show edit update destroy for brains

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :brains, only: [:index, :new, :create]
   devise_for :users
   root to: 'pages#home'
+  resources :brains
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
