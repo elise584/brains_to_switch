2bf13c145b3553dd7666557680f38d82a12c9d66
Elise Dindault <elise.dindault@gmail.com>
2020-05-20 15:15:24 +0200

    modify brain forms to add address

--- app/controllers/brains_controller.rb
+++ app/controllers/brains_controller.rb
@@ -51,8 +51,9 @@ class BrainsController < ApplicationController
 
 
   private
+
   def brain_params
-    params.require(:brain).permit(:name, :description, :category, :price)
+    params.require(:brain).permit(:name, :description, :category, :price, :address)
   end
 
   def set_brain
